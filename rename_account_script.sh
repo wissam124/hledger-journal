95a17f48c57053607c0bf32489ed8843158ecb0d
Jean Dupin <you@exampgile.com>
2026-03-24 14:26:54 +0100

    Improve rename script

--- rename_account_script.sh
+++ rename_account_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# find ~/Syncthing/hledger/ -type f -name "*.journal" -print0 |
-find -E ~/Syncthing/hledger/ -type f -regex ".*/[0-9]{4}/[0-9]{6}\.journal" -print0 |
+DIR_PTA="./"
+OLD_ACCTNAME="Expenses:Misc:Unclassified"
+NEW_ACCTNAME="Expenses:Misc:Test"
+
+find -E $DIR_PTA -type f -regex ".*/[0-9]{4}/[0-9]{6}\.journal" -print0 |
     while IFS= read -r -d '' file; do
         echo "Processing $file"
-        awk -v OLD='Equity:Opening-Balances:GBP' -v NEW='Equity:Opening-Balances' '
+        awk -v OLD=$OLD_ACCTNAME -v NEW=$NEW_ACCTNAME '
     # Start of a transaction when we see a date line
     /^[0-9]{4}-[0-9]{2}-[0-9]{2}([[:space:]].*)?$/ { in_txn=1 }
     # Blank line ends a transaction

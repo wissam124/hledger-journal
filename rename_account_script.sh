bd1b20f9ad5665f2a634e3a1cedafeabc98b7fd6
Jean Dupin <you@exampgile.com>
2026-05-09 14:05:42 +0200

    Rename OfficeLunch -> Lunch

--- rename_account_script.sh
+++ rename_account_script.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 DIR_PTA="./"
-OLD_ACCTNAME="Expenses:Misc:Unclassified"
-NEW_ACCTNAME="Expenses:Misc:Test"
+OLD_ACCTNAME="Expenses:Food:OfficeLunch"
+NEW_ACCTNAME="Expenses:Food:Lunch"
 
-find -E $DIR_PTA -type f -regex ".*/[0-9]{4}/[0-9]{6}\.journal" -print0 |
+find -E $DIR_PTA -type f -regex ".*/[0-9]{4}/([0-9]{6}|[0-9]{4}-(close|closed|open))\.journal" -print0 |
     while IFS= read -r -d '' file; do
         echo "Processing $file"
         awk -v OLD=$OLD_ACCTNAME -v NEW=$NEW_ACCTNAME '
